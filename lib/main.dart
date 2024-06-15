b10ac8fd094291b46262759557d70d7efa324766
山岸健人 <kent@MacBook-Air.local>
2024-06-15 09:27:35 +0900

    やっぱ

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '平成',
+      title: '平成プロフ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Madou',
